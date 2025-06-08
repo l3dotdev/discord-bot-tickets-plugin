5fe98283cf1d53ef697d166405d0c20d24dda6cc
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-06-08 20:40:15 +0100

    Try fix database param type

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 import { definePlugin } from "@l3dev/discord.js-helpers";
+import type { WrappedDrizzle } from "@l3dev/drizzle-helpers";
 
 import { getCommandModules } from "./commands/index.js";
 import { PLUGIN_NAME } from "./constants.js";
+import * as schema from "./db-schema/index.js";
 import { getEventListenerModules } from "./events/index.js";
 import type { Database } from "./logic/repository.js";
 import { TicketChannels } from "./logic/ticket-channels.js";
 import { TicketFields } from "./logic/ticket-fields.js";
 import { Tickets } from "./logic/ticket.js";
 
-type PluginConfig = {
-	db: Database;
+type PluginConfig<TSchema extends typeof schema> = {
+	db: WrappedDrizzle<TSchema>;
 };
 
-export default function botTickets(config: PluginConfig) {
-	const ticketChannels = new TicketChannels(config.db);
-	const ticketFields = new TicketFields(config.db);
-	const tickets = new Tickets(config.db, ticketFields);
+export default function botTickets<TSchema extends typeof schema>(config: PluginConfig<TSchema>) {
+	const db = config.db as unknown as Database;
+
+	const ticketChannels = new TicketChannels(db);
+	const ticketFields = new TicketFields(db);
+	const tickets = new Tickets(db, ticketFields);
 
 	const logic = { ticketChannels, ticketFields, tickets };
 
