01cea1770e49a8c1baa0e4e8750a8b4bd3a421c4
Leon Semmens <35655841+l3dotdev@users.noreply.github.com>
2025-06-08 20:54:08 +0100

    Allow any schema

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import type { WrappedDrizzle } from "@l3dev/drizzle-helpers";
 
 import { getCommandModules } from "./commands/index.js";
 import { PLUGIN_NAME } from "./constants.js";
-import * as schema from "./db-schema/index.js";
 import { getEventListenerModules } from "./events/index.js";
 import type { Database } from "./logic/repository.js";
 import { TicketChannels } from "./logic/ticket-channels.js";
 import { TicketFields } from "./logic/ticket-fields.js";
 import { Tickets } from "./logic/ticket.js";
 
-type PluginConfig<TSchema extends typeof schema> = {
-	db: WrappedDrizzle<TSchema>;
+type PluginConfig = {
+	db: WrappedDrizzle<any>;
 };
 
-export default function botTickets<TSchema extends typeof schema>(config: PluginConfig<TSchema>) {
+export default function botTickets(config: PluginConfig) {
+	// TODO: Try make this type-safe
 	const db = config.db as unknown as Database;
 
 	const ticketChannels = new TicketChannels(db);
